5ccd181be37557d892c9efbf6f93487de6f6d90c
st10068763 <anterozacarias007@gmail.com>
2024-08-13 11:45:13 +0200

    Added a new activity main with tabs and deleted the main activity 1 and renamed the main activity 1 to 2 and deleted the activity i the manifest class.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,8 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.legacy.support.v4)
     implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/myweather/MainActivity.kt
+++ app/src/main/java/com/example/myweather/MainActivity.kt
@@ -1,19 +1,35 @@
 package com.example.myweather
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myweather.ExampleJson2KtKotlin
-import com.example.myweather.databinding.ActivityMainBinding
-import com.google.gson.Gson
-import kotlin.concurrent.thread
+import com.example.myweather.ui.main.SectionsPagerAdapter
+import com.example.myweather.databinding.ActivityMain2Binding
 
-    class MainActivity : AppCompatActivity() {
-        lateinit var binding: ActivityMainBinding
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            binding = ActivityMainBinding.inflate(layoutInflater)
-            setContentView(binding.root)
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = binding.viewPager
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = binding.tabs
+        tabs.setupWithViewPager(viewPager)
+        val fab: FloatingActionButton = binding.fab
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .setAnchorView(R.id.fab).show()
         }
     }
+}
